999d3f0f21eee45548d765fe489fc9968bc23b76
Shubham Gujarathi <48732019+sbgprojects@users.noreply.github.com>
2026-05-26 22:08:23 +0530

    feat: integrate PartnerPathways component into /partners page

--- app/partners/page.tsx
+++ app/partners/page.tsx
@@ -1,15 +1,18 @@
 import type { Metadata } from 'next'
 import siteConfigRaw from '@/data/site/config.json'
-import type { SiteConfig, PartnersEcosystemData, HowWeCreateValueData } from '@/data/types'
+import type { SiteConfig, PartnersEcosystemData, HowWeCreateValueData, PartnerPathwaysData } from '@/data/types'
 import PartnersHero from '@/components/PartnersHero/PartnersHero'
 import PartnersEcosystem from '@/components/PartnersEcosystem/PartnersEcosystem'
 import HowWeCreateValue from '@/components/HowWeCreateValue/HowWeCreateValue'
+import PartnerPathways from '@/components/PartnerPathways/PartnerPathways'
 import ecosystemDataRaw from '@/data/partners/ecosystem.json'
 import howWeCreateValueDataRaw from '@/data/partners/how-we-create-value.json'
+import partnerPathwaysDataRaw from '@/data/partners/partner-pathways.json'
 
 const siteConfig = siteConfigRaw as SiteConfig
 const ecosystemData = ecosystemDataRaw as PartnersEcosystemData
 const howWeCreateValueData = howWeCreateValueDataRaw as HowWeCreateValueData
+const partnerPathwaysData = partnerPathwaysDataRaw as PartnerPathwaysData
 
 export const metadata: Metadata = {
   title: 'Bitcoin Mining Partnerships | Sustainable BTC',
@@ -36,6 +39,7 @@ export default function PartnersPage() {
       <PartnersHero />
       <PartnersEcosystem data={ecosystemData} />
       <HowWeCreateValue data={howWeCreateValueData} />
+      <PartnerPathways data={partnerPathwaysData} />
     </main>
   )
 }
